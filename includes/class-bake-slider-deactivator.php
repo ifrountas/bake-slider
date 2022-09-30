ddf6ab13fbccbe5bd45a01cf696b79b68cc455a0
Iakovos Frountas <iakovosfrountas@gmail.com>
2022-09-30 12:04:36 +0300

    initial commit

---
+++ includes/class-bake-slider-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://bakemywp.com/
+ * @since      1.0.0
+ *
+ * @package    Bake_Slider
+ * @subpackage Bake_Slider/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Bake_Slider
+ * @subpackage Bake_Slider/includes
+ * @author     Iakovos Frountas <hello@bakemywp.com>
+ */
+class Bake_Slider_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
---
+++ includes/class-bake-slider-i18n.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Define the internationalization functionality
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @link       https://bakemywp.com/
+ * @since      1.0.0
+ *
+ * @package    Bake_Slider
+ * @subpackage Bake_Slider/includes
+ */
+
+/**
+ * Define the internationalization functionality.
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @since      1.0.0
+ * @package    Bake_Slider
+ * @subpackage Bake_Slider/includes
+ * @author     Iakovos Frountas <hello@bakemywp.com>
+ */
+class Bake_Slider_i18n {
+
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+	public function load_plugin_textdomain() {
+
+		load_plugin_textdomain(
+			'bake-slider',
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
+
+	}
+
+
+
+}
